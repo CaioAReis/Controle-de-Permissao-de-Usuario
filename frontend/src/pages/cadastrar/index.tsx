78fa54cef90b71c00e4a7be37bb1131964dbbe04
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-03-25 15:26:06 -0300

    Tentando fazer a integração com o axios

--- frontend/src/pages/cadastrar/index.tsx
+++ frontend/src/pages/cadastrar/index.tsx
@@ -1,26 +1,87 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useHistory } from "react-router-dom";
 
+import api from '../../services/api';
 import './style.css';
 
 export function Cadastrar() {
+
+    const [nome, setNome] = useState('');
+    const [login, setLogin] = useState('');
+    const [senha, setSenha] = useState('');
+    const [email, setEmail] = useState('');
+    const [data_nascimento, setData] = useState('');
+    const [status, setStatus] = useState('');
+
+    const history = useHistory();
+
+    function handleCadastro(e: any) {
+        e.preventDefault();
+
+        const dados = {
+            nome,
+            login,
+            senha,
+            email,
+            data_nascimento,
+            status
+        };
+
+        try {
+            const response = api.post('/usuario', dados);
+            alert(`Conta criada com sucesso.`);
+            history.push('/');
+        } catch(err) {
+            alert('Ocorreu um erro no cadastro.');
+        }
+    }
+
     return(
         <div className="containerXY">
+
             <section className='form-container'>
-                <form className='cadastro' action="" method="post">
+                <form className='cadastro' onSubmit={handleCadastro}>
                     <h1>Cadastro de usuário</h1>
 
-                    <input name='nome' placeholder='Nome'/>
-                    <input name='Login' placeholder='Login'/>
-                    <input type='password' name='senha' placeholder='Senha'/>
-                    <input type='email' name='email' placeholder='Email'/>
+                    <input 
+                        name='nome' 
+                        placeholder='Nome'
+                        value={nome}
+                        onChange={ e => setNome(e.target.value)}/>
+
+                    <input 
+                        name='Login' 
+                        placeholder='Login'
+                        value={login}
+                        onChange={ e => setLogin(e.target.value)}/>
+
+                    <input 
+                        type='password' 
+                        placeholder='Senha'
+                        value={senha}
+                        onChange={ e => setSenha(e.target.value)}/>
+
+                    <input 
+                        type='email' 
+                        placeholder='Email'
+                        value={email}
+                        onChange={ e => setEmail(e.target.value)}/>
                 
                     <div>
                         <div style={{fontSize: '16px', marginBottom: '10px' }} >Data de nascimento:</div>
-                        <input type='date' name='data_nascimento' placeholder='Data nascimento'/>
+                        <input 
+                            type='date' 
+                            placeholder='Data nascimento'
+                            value={data_nascimento}
+                            onChange={ e => setData(e.target.value)}/>
                     </div>
 
                     <div className='last-input'>
-                        <input name='status' maxLength={1} placeholder='Status: A, B etc.'/> 
+                        <input 
+                            maxLength={1} 
+                            placeholder='Status: A, B etc.'
+                            value={status}
+                            onChange={ e => setStatus(e.target.value)}/> 
                         <span>-</span>
                         <button className='button' type="submit">Cadastar</button>
                     </div>
